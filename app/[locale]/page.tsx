a67fed42c46e717f418fd1fb9c9bc5e7d5cec495
22 <60903333+nini22P@users.noreply.github.com>
2023-07-29 18:40:03 +0800

    Change: add Sponsor and  Contributor (#26)

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,18 +1,23 @@
 'use client'
 
-import styles from './Home.module.css'
 import WebGALGames from './components/WebGALGames/WebGALGames'
 import TopVisual from './components/TopVisual/TopVisual'
 import Features from './components/Features/Features'
 import Feat from './components/Feat/Feat'
+import Sponsor from './components/Sponsor/Sponsor'
+import Contributor from './components/Contributor/Contributor'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <TopVisual />
       <Feat />
       <WebGALGames />
       <Features />
+      <div className={'max-w-screen-xl mx-auto px-4 py-8 gap-4 grid grid-cols-2 max-md:grid-cols-1'}>
+        <Sponsor />
+        <Contributor />
+      </div>
     </main >
   )
 }
---
+++ data/sponsorsData.ts
@@ -0,0 +1,28 @@
+import { Sponsors } from '@/types'
+
+export const sponsors: Sponsors = {
+  special: null,
+  platinum: null,
+  gold: [
+    {
+      name: 'T2-official (T2)',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/91712707?v=4',
+      url: 'https://github.com/T2-official',
+    },
+  ],
+  silver: [
+    {
+      name: 'IdrilK',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/103700780?v=4',
+      url: 'https://github.com/IdrilK',
+    },
+  ],
+  sponsors: [
+    {
+      name: 'generalfreed (Yuji Sakai)',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/71590526?v=4',
+      url: 'https://github.com/generalfreed',
+    },
+  ]
+
+}
