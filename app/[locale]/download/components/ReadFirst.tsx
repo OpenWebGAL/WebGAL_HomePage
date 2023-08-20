262abb950330d58fcb86a1d7e9dabe5558f5fdfb
22 <60903333+nini22P@users.noreply.github.com>
2023-08-20 21:30:23 +0800

    Fix document url

--- app/[locale]/download/components/ReadFirst.tsx
+++ app/[locale]/download/components/ReadFirst.tsx
@@ -13,7 +13,7 @@ const ReadFirst = () => {
   return (
     <div className={styles.card}>
       <div className={'text-sm font-light space-y-1'}>
-        <p>{t('firstUse.part0')} <Link href={docsRedirect('/guide')} target={'_blank'}>{t('firstUse.part1')}</Link> {t('firstUse.part2')}</p>
+        <p>{t('firstUse.part0')} <Link href={docsRedirect('/')} target={'_blank'}>{t('firstUse.part1')}</Link> {t('firstUse.part2')}</p>
         <p>{t('getHelp')}</p>
         <p>{t('webGALScriptBasics.part0')} <Link href={'https://marketplace.visualstudio.com/items?itemName=c6h5-no2.webgal-script-basics'} target={'_blank'}>WebGAL Script Basics</Link> {t('webGALScriptBasics.part1')}</p>
       </div>
