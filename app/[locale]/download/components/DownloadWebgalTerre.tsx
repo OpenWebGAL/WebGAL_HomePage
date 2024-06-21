3435db0ee3c4f07eeb4e1078a0a8317e9d4342f8
Mahiru <Mahiru_@outlook.com>
2024-06-21 23:58:47 +0800

    feat: enhance download page

--- app/[locale]/download/components/DownloadWebgalTerre.tsx
+++ app/[locale]/download/components/DownloadWebgalTerre.tsx
@@ -3,10 +3,10 @@
 import Button from '@/components/Button/Button'
 import useWebgalTerreAssets from '@/hooks/useWebgalTerreAssets'
 import Link from 'next/link'
-import { RiGithubFill, RiMicrosoftFill, RiAppleFill, RiUbuntuFill } from 'react-icons/ri'
+import {RiGithubFill, RiMicrosoftFill, RiAppleFill, RiUbuntuFill} from 'react-icons/ri'
 import styles from '../Download.module.css'
-import { useLocale, useTranslations } from 'next-intl'
-import { i18n } from '@/i18n'
+import {useLocale, useTranslations} from 'next-intl'
+import {i18n} from '@/i18n'
 
 const DownloadWebgalTerre = () => {
 
@@ -28,20 +28,20 @@ const DownloadWebgalTerre = () => {
         : null
 
   const platformMap = [
-    { platform: 'windows', icon: <RiMicrosoftFill />, label: t('downloadWindows') },
-    { platform: 'windowsSetup', icon: <RiMicrosoftFill />, label: t('downloadWindowsSetup') },
-    { platform: 'windowsArm64', icon: <RiMicrosoftFill />, label: t('downloadWindowsArm64') },
-    { platform: 'windowsArm64Setup', icon: <RiMicrosoftFill />, label: t('downloadWindowsArm64Setup') },
-    { platform: 'macos', icon: <RiAppleFill />, label: t('downloadMacos') },
-    { platform: 'linux', icon: <RiUbuntuFill />, label: t('downloadLinux') },
-    { platform: 'linuxArm64', icon: <RiUbuntuFill />, label: t('downloadLinuxArm64') },
+    {platform: 'windows', icon: <RiMicrosoftFill/>, label: t('downloadWindows')},
+    {platform: 'windowsSetup', icon: <RiMicrosoftFill/>, label: t('downloadWindowsSetup')},
+    {platform: 'windowsArm64', icon: <RiMicrosoftFill/>, label: t('downloadWindowsArm64')},
+    {platform: 'windowsArm64Setup', icon: <RiMicrosoftFill/>, label: t('downloadWindowsArm64Setup')},
+    {platform: 'macos', icon: <RiAppleFill/>, label: t('downloadMacos')},
+    {platform: 'linux', icon: <RiUbuntuFill/>, label: t('downloadLinux')},
+    {platform: 'linuxArm64', icon: <RiUbuntuFill/>, label: t('downloadLinuxArm64')},
   ]
 
   const downloadUrls = webgalTerreAssets?.downloadUrl
     .map((item) => {
       const test = platformMap.find(platform => item.platform === platform.platform)
       if (test)
-        return { url: item.url, ...test }
+        return {url: item.url, ...test}
     })
 
   const isZh = locale === 'zh-cn'
@@ -54,50 +54,53 @@ const DownloadWebgalTerre = () => {
         {/* <div className={styles.corner}>
           <p className={`${styles['corner-text']} bg-terre`}>{t('recommend')}</p>
         </div> */}
-        <p><strong>{t('version')}:</strong> {webgalTerreAssets?.version ? webgalTerreAssets?.version : t('fetching')}</p>
-        <p><strong>{t('releaseTime')}:</strong> {webgalTerreAssets?.releaseTime ? webgalTerreAssets?.releaseTime.split('T')[0] : t('fetching')} </p>
+        <p><strong>{t('version')}:</strong> {webgalTerreAssets?.version ? webgalTerreAssets?.version : t('fetching')}
+        </p>
+        <p>
+          <strong>{t('releaseTime')}:</strong> {webgalTerreAssets?.releaseTime ? webgalTerreAssets?.releaseTime.split('T')[0] : t('fetching')}
+        </p>
         <div>
           <p><strong>{t('releaseNote')}:</strong> {!(releaseNote) && t('fetching')}</p>
           {
             releaseNote &&
-            < div dangerouslySetInnerHTML={{ __html: releaseNote }} className={styles['release-note']} />
+              < div dangerouslySetInnerHTML={{__html: releaseNote}} className={styles['release-note']}/>
           }
         </div>
       </div>
       <div className={styles['card-button-gourp']}>
         <Button terre>
-          <Link href={webgalTerreUrl} target={'_blank'}><RiGithubFill />{t('gitHubReleases')}</Link>
+          <Link href={webgalTerreUrl} target={'_blank'}><RiGithubFill/>{t('gitHubReleases')}</Link>
         </Button>
         {
           downloadUrls &&
           downloadUrls.map((item) =>
             item?.url &&
-            <Button terre key={item.platform}>
-              <Link href={item.url} target='_top'>{item.icon}{item.label}</Link>
-            </Button>
+              <Button terre key={item.platform}>
+                  <Link href={item.url} target='_top'>{item.icon}{item.label}</Link>
+              </Button>
           )
         }
 
       </div>
       {
         isZh && downloadUrls && <div className={styles.cndownload}>
-          <details className='space-y-4'>
-            <summary className='cursor-pointer text-terre select-none'>
-              {t('otherDownloadLinkInfo')}
-            </summary>
-            <div className={styles['card-button-gourp']}>
-              {
-                downloadUrls &&
-                downloadUrls.map((item) =>
-                  item?.url &&
-                  <Button terre key={item.platform}>
-                    <Link href={ghproxyStr + item.url} target='_top'>{item.icon}{item.label}</Link>
-                  </Button>
-                )
-              }
-            </div>
-          </details>
-        </div>
+          {/*<details className='space-y-4'>*/}
+              <div className='text-terre select-none'>
+                {t('otherDownloadLinkInfo')}
+              </div>
+              <div className={styles['card-button-gourp']}>
+                {
+                  downloadUrls &&
+                  downloadUrls.map((item) =>
+                    item?.url &&
+                      <Button terre key={item.platform}>
+                          <Link href={ghproxyStr + item.url} target='_top'>{item.icon}{item.label}</Link>
+                      </Button>
+                  )
+                }
+              </div>
+          {/*</details>*/}
+          </div>
       }
     </div>
   )
--- app/[locale]/download/components/ReadFirst.tsx
+++ app/[locale]/download/components/ReadFirst.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useTranslations } from 'next-intl'
+import {useTranslations} from 'next-intl'
 import styles from '../Download.module.css'
 import Link from 'next/link'
 import useRedirect from '@/hooks/useRedirect'
@@ -8,14 +8,24 @@ import useRedirect from '@/hooks/useRedirect'
 const ReadFirst = () => {
 
   const t = useTranslations('download.readFirst')
-  const { docsRedirect } = useRedirect()
+  const {docsRedirect} = useRedirect()
 
   return (
     <div className={styles.card}>
       <div className={'text-sm font-light space-y-1'}>
-        <p>{t('firstUse.part0')} <Link href={docsRedirect('/')} target={'_blank'}>{t('firstUse.part1')}</Link> {t('firstUse.part2')}</p>
-        <p>{t('getHelp')}</p>
-        <p>{t('webGALScriptBasics.part0')} <Link href={'https://marketplace.visualstudio.com/items?itemName=c6h5-no2.webgal-script-basics'} target={'_blank'}>WebGAL Script Basics</Link> {t('webGALScriptBasics.part1')}</p>
+        <div className={styles.hlrf}>
+          <p>{t('firstUse.part0')}
+            <Link className={styles.link} href={docsRedirect('/')}
+                  target={'_blank'}>
+              {t('firstUse.part1')}
+            </Link>
+            {t('firstUse.part2')}
+          </p>
+        </div>
+        <p className={styles.p}>{t('getHelp')}</p>
+        <p className={styles.p}>{t('webGALScriptBasics.part0')} <Link
+          href={'https://marketplace.visualstudio.com/items?itemName=Xuran1783558957.webgal-for-vscode'} target={'_blank'}>WebGAL
+          for VSCode</Link> {t('webGALScriptBasics.part1')}</p>
       </div>
     </div>
   )
